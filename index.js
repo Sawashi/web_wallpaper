f60326839a6623c4775b5d24595ad1dcc599d659
tienhoangg <hoangtrantien202@gmail.com>
2022-05-28 22:23:31 +0700

    abc

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const http = require('http');
-const post = 3000;
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //response to the client
-  res.end(); //end the response
-}).listen(port); //the server listens on port 3000
-console.log(`Server is running at port ${port}`);
+window.alert("ok nha");
